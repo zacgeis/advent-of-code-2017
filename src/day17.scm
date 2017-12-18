28f0c2c77bccf4b5c603f43acd0874d162fe886a
zacgeis <zacgeis@gmail.com>
2017-12-17 23:48:31 -0600

    day17 complete

--- src/day17.scm
+++ src/day17.scm
@@ -2,12 +2,12 @@
 (require-extension srfi-1)
 
 (define (insert lst i v)
-    (append (take lst (+ i 1)) (list v) (take-right lst (- (- (length lst) 1) i))))
+  (append (take lst (+ i 1)) (list v) (take-right lst (- (- (length lst) 1) i))))
 
 (define (gen-lst n jump)
   (let loop ((i 1) (v 2) (l '(0 1)))
     (if (> v n) l
-      (let ((t (remainder (+ i jump) (length l))))
+      (let ((t (remainder (+ i jump) v)))
         (loop (+ t 1) (+ v 1) (insert l t v))))))
 
 (define (find-ind lst v)
@@ -18,5 +18,14 @@
 (define lst (gen-lst 2017 348))
 (print (list-ref lst (+ (find-ind lst 2017) 1)))
 
-; (define lst (gen-lst 5000 348)
-; (print (list-ref lst (+ (find-ind lst 2017) 1)))
+(define (scoped-insert lst i v)
+  (if (= i 0) (list (car lst) v) lst))
+
+(define (scoped-gen n jump)
+  (let loop ((i 1) (v 2) (l '(0 1)))
+    (if (> v n) l
+      (let ((t (remainder (+ i jump) v)))
+        (loop (+ t 1) (+ v 1) (scoped-insert l t v))))))
+
+(define lst (scoped-gen 50000000 348))
+(print lst)
