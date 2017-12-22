58c7ca457ef50b55c5bc7bc7183a6f745da78fdf
zacgeis <zacgeis@gmail.com>
2017-12-22 10:58:36 -0600

    day21 complete

--- src/day21.scm
+++ src/day21.scm
@@ -1,8 +1,6 @@
 (require-extension utils)
 (require-extension srfi-1)
 
-;'((1 2 3 4) (5 6 7 8) (9 10 11 12) (13 14 15 16))
-; (define input '((1 2) (4 3)))
 (define (print-g g)
   (for-each print g)
   (newline))
